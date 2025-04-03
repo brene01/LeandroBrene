2cb5349d81402c0422cc665c16c28882c8a3e52b
Leandro Brene <brene.porto@gmail.com>
2025-04-03 20:54:23 -0300

    Create tabuada.c

---
+++ C/tabuada.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int num, i;
+	
+	printf("Digite um numero para ver sua tabuada: ");
+	scanf("%d", &num);
+	
+	printf("==============================\n");
+	printf("	Tabuada do %d\n", num);
+	printf("==============================\n");
+	
+	for (i= 0; i<=10; i++){
+		printf("| %d x %d = %d |\n", num, i, num * i);
+	}
+	
+	printf("==============================\n");
+			
+	return 0;
+}
