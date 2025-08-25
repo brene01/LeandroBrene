b87a7e8a7d841a2f3a783138f76021e41435f590
Leandro Brene <brene.porto@gmail.com>
2025-08-25 20:26:41 -0300

    struct.c

--- Estrutura de Dados/struct.c
+++ Estrutura de Dados/struct.c
@@ -1,5 +1,4 @@
- Online C compiler to run C program online
-#include stdio.h
+#include <stdio.h>
 typedef struct{
     char nome[40];
     int idade;
@@ -7,22 +6,35 @@ typedef struct{
 }contribuinte;
 int main(){
     int n;
-    printf(Digite a quantidade de contribuintesn);
-    scanf(%d,&n);
+    printf("Digite a quantidade de contribuintes:\n");
+    scanf("%d",&n);
     contribuinte pessoas[n];
     int menor=0;
     int isentas=0;
-    for (int i=0;in;i++){
-        printf(n Contribuinte %d, i+1);
-        printf(nNome );
-        scanf(%39s, pessoas[i].nome);
-        printf(nIdade );
-        scanf(%d, pessoas[i].idade);
-        printf(nRenda Anual );
-        scanf(%f, pessoas[i].renda);
-    if (pessoas[i].idade18)
+    for (int i=0;i<n;i++){
+        printf("\n****** Contribuinte %d********", i+1);
+        printf("\nNome: ");
+        scanf("%39s", pessoas[i].nome);
+        printf("\nIdade: ");
+        scanf("%d", &pessoas[i].idade);
+        printf("\nRenda Anual: ");
+        scanf("%f", &pessoas[i].renda);
+    if (pessoas[i].idade<18)
         menor++;
-    if (pessoas[i].renda= 28559.70)
+    if (pessoas[i].renda<= 28559.70)
         isentas++;
     }
+    printf("\n********************Resultados**********************");
+    printf("\nTemos %d pessoas menores de idade", menor);
+    printf("\nTemos %d contribuintes isentos de IR", isentas);
+    printf("\n");
+    printf("\n********************DADOS PESSOAIS*******************");
+    printf("\n");
+    for(int i=0;i<n;i++){
+        printf("-----Pessoa %d ----- ",i+1);
+        printf("Nome:%s\n", pessoas[i].nome);
+        printf("Idade:%d\n", pessoas[i].idade);
+        printf("Renda:%.2f\n", pessoas[i].renda);
+    }
+    return 0;
 }
